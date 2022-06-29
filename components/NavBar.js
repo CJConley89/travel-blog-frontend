6065ea45185afad420e284931b3cf3fca0da97d1
Chris Conley <conley.christopher@hotmail.com>
2022-06-29 17:47:20 -0500

    commit

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import logo from '../public/travel-blog-logo.jpg'
 const NavBar = () => {
     return (
         <nav className='nav-container'>
             <div className='nav-item-container' id='nav-image-home'>
                 <Link href='/'>
-                    <img 
+                    <Image 
                         className='nav-bar-image'
                         src={logo}
                         alt='Travel Blog Logo'
                     />
                 </Link>
+            </div>
+            <div className='nav-item-container' id='nav-image-src-link'>
                 <small><a href='https://www.freepik.com/vectors/explore'>Image from <strong>pch.vector</strong></a></small>
             </div>
             <div className='nav-item-container' id='credits'>
